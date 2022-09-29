66dd63a37d8a5982fb05275dcbad329338929130
Lucas Toschini <lucas.toschini@live.fr>
2022-09-29 16:23:44 +0200

    navbar + banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import HeaderBand from './Components/HeaderBand.js';
+import Navbar from './Components/Navbar.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderBand />
+      <Navbar/>
     </div>
   );
 }
---
+++ src/Components/HeaderBand.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/* header sous forme de fonction */
+export default function HeaderBand(props) {
+  return (
+    <header>
+      <div class="banner">
+          <input class="btn-search"placeholder='Rechercher...'></input>
+      </div>
+    </header>
+  )
+}
